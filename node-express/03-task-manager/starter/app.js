31d7a181fab6dc3340665d8d818df07ebd881d37
unknown <usern5949@gmail.com>
2024-02-23 03:11:50 +0530

    Added basic routes, controllers

--- node-express/03-task-manager/starter/app.js
+++ node-express/03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3000;
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(port, () => {
+  console.log("App is listening on port " + port);
+});
