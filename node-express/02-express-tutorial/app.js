ea010e8321dde0b476c7c843c2d04460f96ed6c0
unknown <usern5949@gmail.com>
2024-02-22 21:15:48 +0530

    Added form data apis

--- node-express/02-express-tutorial/app.js
+++ node-express/02-express-tutorial/app.js
@@ -228,15 +228,26 @@
 //   console.log("Server is listening on port 5000...");
 // });
 
+//Dynamic responses
 const express = require("express");
 const app = express();
 let { people } = require("./data");
 app.use(express.static("./methods-public"));
 // app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (name) return res.status(201).json({ success: true, person: name });
+  else
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
 });
 
 app.post("/login", (req, res) => {
