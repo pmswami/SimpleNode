5f737b5556512fed16d4ea9b854a6b5149a663ce
unknown <usern5949@gmail.com>
2024-02-24 03:38:07 +0530

    Added basic routes

--- node-express/04-store-api/starter/app.js
+++ node-express/04-store-api/starter/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+console.log("04 Store API");
+require("dotenv").config();
+
+//async error
+
+const express = require("express");
+const app = express();
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+//middlewares
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store APIs</h1><a href='/api/v1/products'>Products Route</a>");
+});
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
