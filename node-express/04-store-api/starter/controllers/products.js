1b6ad8a04b99ff4d3e8d5ca837956fe80d635de6
unknown <usern5949@gmail.com>
2024-02-24 22:25:42 +0530

    Added query search for get APIs

--- node-express/04-store-api/starter/controllers/products.js
+++ node-express/04-store-api/starter/controllers/products.js
@@ -7,7 +7,20 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
