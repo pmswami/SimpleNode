76cee7e91f7cba27179b708bde17c3b380ef4bc1
unknown <usern5949@gmail.com>
2024-02-25 01:02:06 +0530

    Added sort functionality to APIs

--- node-express/04-store-api/starter/controllers/products.js
+++ node-express/04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -19,7 +19,15 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    console.log(sort);
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("-createdAt");
+  }
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
