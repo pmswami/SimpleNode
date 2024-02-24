7817a54dd028f83564034f7d71b091a6663594c3
unknown <usern5949@gmail.com>
2024-02-25 01:07:45 +0530

    Added field selection to APIs

--- node-express/04-store-api/starter/controllers/products.js
+++ node-express/04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -26,6 +26,10 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort("-createdAt");
+  }
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
