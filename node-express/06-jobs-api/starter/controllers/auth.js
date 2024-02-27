65232278acb6a5f78250901d823f57e54bcb9a9a
unknown <usern5949@gmail.com>
2024-02-27 12:09:21 +0530

    Added jwt token in register response

--- node-express/06-jobs-api/starter/controllers/auth.js
+++ node-express/06-jobs-api/starter/controllers/auth.js
@@ -2,6 +2,8 @@ const { BadRequestError } = require("../errors");
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const register = async (req, res) => {
   // console.log(req.body);
@@ -10,12 +12,23 @@ const register = async (req, res) => {
   // if (!name || !password || !email) {
   //   throw new BadRequestError("Please provide name, email and password");
   // }
+
   const { name, email, password } = req.body;
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const tempUser = { name, email, password: hashedPassword };
+  // const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashedPassword };
+  const tempUser = { name, email, password };
+  //create user with password encrypted
   const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json({ user });
+  //generate token
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
--- node-express/06-jobs-api/starter/models/User.js
+++ node-express/06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -22,4 +23,17 @@ const UserSchema = mongoose.Schema({
     minlength: 6,
   },
 });
+
+//mongoose middleware for password encryption
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// UserSchema.pre("save", async function () {
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
+
 module.exports = mongoose.model("User", UserSchema);
