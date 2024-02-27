abdc4809f21b9f41db0dd2900c0ae1869756baef
unknown <usern5949@gmail.com>
2024-02-27 12:19:54 +0530

    Added model methods

--- node-express/06-jobs-api/starter/controllers/auth.js
+++ node-express/06-jobs-api/starter/controllers/auth.js
@@ -21,14 +21,18 @@ const register = async (req, res) => {
   //create user with password encrypted
   const user = await User.create({ ...tempUser });
   //generate token
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "30d",
-    }
-  );
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  const token = user.createJWT();
+  // const token = jwt.sign(
+  //   { userId: user._id, name: user.name },
+  //   process.env.JWT_SECRET,
+  //   {
+  //     expiresIn: "30d",
+  //   }
+  // );
+  res
+    .status(StatusCodes.CREATED)
+    // .json({ user: { name: user.name }, token });
+    .json({ user: { name: user.getName() }, token });
 };
 
 const login = async (req, res) => {
